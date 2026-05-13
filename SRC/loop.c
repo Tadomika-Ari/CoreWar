62771602be69b73b3c0d971eb25633c598ded62e
Lucas EECKHOUTTE <lucas1.eeckhoutte@epitech.eu>
2026-05-13 10:20:44 +0200

    [+] add function for delibiration

--- SRC/loop.c
+++ SRC/loop.c
@@ -7,6 +7,21 @@
 
 #include "../include/robotfactorie.h"
 
+int delibiration(int alive, champ_t *winner)
+{
+    if (alive == 1) {
+        my_printf("The player %d(%s) has won.\n",
+            winner->name_champ, winner->file_champ);
+        return 1;
+    }
+    if (alive == 0) {
+        my_printf("No champion alive.\n");
+        return 1;
+    }
+    my_printf("No winner.\n");
+    return 1;
+}
+
 int check_is_dead(int life, ll_t *list_champ, coreware_t *core)
 {
     champ_t *champ = NULL;
@@ -20,15 +35,7 @@ int check_is_dead(int life, ll_t *list_champ, coreware_t *core)
             winner = champ;
         }
     }
-    if (alive <= 1) {
-        if (winner != NULL)
-            my_printf("The player %d(%s) has won.\n",
-                winner->name_champ, winner->file_champ);
-        else
-            my_printf("No champion alive.\n");
-        return 1;
-    }
-    return 0;
+    return delibiration(alive, winner);
 }
 
 int check_win(coreware_t *core, ll_t *list_champ)
@@ -45,11 +52,6 @@ int loop(coreware_t *core, ll_t *list_champ)
     see_struct(list_champ);
     set_champ_dead(list_champ, core, 0);
     for (int cycle = 1; cycle_to_die >= 0; cycle++) {
-        if (check_is_dead(life, list_champ, core) == 1) {
-            printf("%d", cycle);
-            return 0;
-        }
-        set_champ_dead(list_champ, core, 1);
         cycle_to_die = cycle_to_die - delta;
     }
     check_win(core, list_champ);
