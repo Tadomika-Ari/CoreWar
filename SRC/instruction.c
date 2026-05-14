5af4cd843c67904b2865501ce94b3bd4db38454b
Lucas EECKHOUTTE <lucas1.eeckhoutte@epitech.eu>
2026-05-14 17:17:31 +0200

    [+] add start function for instruction

---
+++ SRC/instruction.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** CoreWar
+** File description:
+** instruction
+*/
+
+#include "../include/robotfactorie.h"
+
+int instruction(coreware_t *core, ll_t *list_champ, uint8_t *arena)
+{
+    return 0;
+}
